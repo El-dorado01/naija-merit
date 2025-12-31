5f61078d3466cc2b727f8b5a75583551f2f8537a
El-dorado01 <doctor.eldorado@gmail.com>
2025-12-31 16:30:20 +0100

    vercel config done

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,8 @@
 // src/prisma/prisma.service.ts
 
 import { Injectable, OnModuleInit } from '@nestjs/common';
-// import { PrismaClient } from '../generated/prisma'; // Your custom output path
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../generated/prisma';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
